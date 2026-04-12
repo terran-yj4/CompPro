9287f01e005d029637427abd5ea5ac0d1cccff79
よう <yojuggler4040@gmail.com>
2026-04-12 13:57:41 +0900

    fix: temp.py

--- temp.py
+++ temp.py
@@ -5,10 +5,10 @@ from random import randint,choice,shuffle
 #shuffle(x)可変シーケンスxの要素をシャッフル
 from copy import deepcopy
 from collections import Counter,defaultdict,deque
-from itertools import accumulate,combinations,permutations
-#accumulate(a)シーケンスaから累積イテレータを生成、通常はリスト化して前に[0]を置いて累積和に使用
-#combinations(a,k)シーケンスaからk個を選ぶ 組み合わせイテレータ
-#permutations(a,k)シーケンスaからk個を選ぶ 順列イテレータ
+from itertools import accumulate,combinations,permutations,product,combinations_with_replacement
+#accumulate(l) 累積和に使用 for v in accumulate(l): print(v)
+#combinations(seq,k) シーケンスseqからk個を選ぶ 組み合わせイテレータ
+#permutations(seq,k) シーケンスseqからk個を選ぶ 順列イテレータ
 from math import ceil,floor,sqrt,pi,factorial,gcd,log,log10,log2,inf
 #ceil切り上げ、floor切り捨て、sqrt平方根、factorial階乗
 input = lambda: sys.stdin.readline().rstrip("\r\n")
---
+++ temp2.py
@@ -0,0 +1,30 @@
+import os,sys,random,threading
+from random import (
+    randint,            # randint(a,b)          [a,b]の範囲からランダムに数を選択
+    choice,             # choice(seq)           seqからランダムに要素を選択 (seqはlist, str, tuple型)
+    shuffle             # shuffle(x)            可変シーケンスxの要素をシャッフル xを書き換え 成功時の返り値はNone
+)
+from copy import (
+    deepcopy            # x.deepcopy()          オリジナルとコピー先で別のオブジェクトとして扱われる 
+)
+from collections import (
+    Counter,            # Counter([1, 2, 2])    数を数えるときに便利 Counter({2: 2, 1: 1})を生成
+    defaultdict,        # defaultdict(int)      キー生成のいらないdict 初期値0で自動で初期化してくれる defaultdict(lambda: defaultdict(int))
+    deque               # q = deque()           キュー
+)
+from itertools import ( # これらは
+    accumulate,         # accumulate(l)         累積和の時使える            for a in accumulate(l): print(a)
+    combinations,       # combinations(seq, k)  seqからk個選ぶ組み合わせ    for i in combinations(l, 2): print(i)
+    permutations,       # permutations(seq, k)  seqからk個並べる並び方      for i in permutations(l, 2): print(i)
+    product,            # product(seq1, seq2)   直積を求める                product([1, 2], [3, 4]) -> [(1, 3), (1, 4), (2, 3), (2, 4)]
+    combinations_with_replacement
+)
+
+l = [1, 2, 4, 8]
+l2 = [1, 2, 4, 8]
+l3 = [1, 2, 4, 8]
+
+p = product(l, l2, l3)
+print(p)
+for pp in p:
+    print(pp)
